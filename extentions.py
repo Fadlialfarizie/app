d161ac085eb5c80e9bf30e270693568bd16e87b8
Fadli Alfarisi <fadlialfarizie99@gmail.com>
2026-01-30 20:38:50 +0700

    fix: correct CORS initialization in extentions.py

--- extentions.py
+++ extentions.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask_cors import CORS
 
 
+cors = CORS()
+
 def init_extentions(app: Flask):
-    CORS.init_app(app,
+    cors.init_app(app,
                   supports_credentials=True,
                   origins=["http://localhost:8158","http://localhost:8159","http://localhost:8160"]
                     )
