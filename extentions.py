67328d1f5d8bd3567d54a522f00c605da9c23b52
Fadli Alfarisi <fadlialfarizie99@gmail.com>
2026-01-30 20:30:13 +0700

    fix(cors): add additional localhost origins

--- extentions.py
+++ extentions.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 def init_extentions(app: Flask):
     CORS.init_app(app,
                   supports_credentials=True,
-                  origins=["http://localhost:8158","http://localhost:8159","http://localhost:8159"]
+                  origins=["http://localhost:8158","http://localhost:8159","http://localhost:8160"]
                     )
 
 
