688ea3e91c8ae9c0f1b5b6ea7efaf373b5c234fb
Fadli Alfarisi <fadlialfarizie99@gmail.com>
2026-01-30 10:26:51 +0700

    fix(cors): allow localhost wildcard

--- extentions.py
+++ extentions.py
@@ -3,6 +3,9 @@ from flask_cors import CORS
 
 
 def init_extentions(app: Flask):
-    CORS.init_app(app, origins=["http://localhost:*"])
+    CORS.init_app(app,
+                  supports_credentials=True,
+                  origins=["http://localhost:8158","http://localhost:8159","http://localhost:8159"]
+                    )
 
 
